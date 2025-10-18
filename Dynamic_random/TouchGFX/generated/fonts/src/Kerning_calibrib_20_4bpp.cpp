ff99876debb2dab708d392ccc2edd35cd3b95597
Tiago Gomes <tejotago@gmail.com>
2025-10-18 16:31:01 +0100

    Include generated folder and update .gitignore

---
+++ Dynamic_random/TouchGFX/generated/fonts/src/Kerning_calibrib_20_4bpp.cpp
@@ -0,0 +1,8 @@
+#include <touchgfx/Font.hpp>
+
+FONT_KERNING_LOCATION_FLASH_PRAGMA
+KEEP extern const touchgfx::KerningNode kerning_calibrib_20_4bpp[] FONT_KERNING_LOCATION_FLASH_ATTRIBUTE = {
+    { 0x004C, -1 },  // (First char = [0x004C, L], Second char = [0x0054, T], Kerning dist = -1)
+    { 0x004F, -1 },  // (First char = [0x004F, O], Second char = [0x0058, X], Kerning dist = -1)
+    { 0x004C, -1 },  // (First char = [0x004C, L], Second char = [0x0077, w], Kerning dist = -1)
+};
